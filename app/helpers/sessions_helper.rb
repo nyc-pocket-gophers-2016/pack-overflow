7053e28900bc7fbdf9a53f765991c29b3cd9e3e8
notjoeyoo <joseph_yoo@brown.edu>
2016-06-02 16:29:40 -0400

    fix controllers

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,11 @@
-def current_user
-  session[:user_id] ? User.find(session[:user_id]) : nil
-end
+helpers do
 
-def logged_in?
-  !!current_user
+  def current_user
+    session[:user_id] ? User.find_by(id: session[:user_id]) : nil
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
