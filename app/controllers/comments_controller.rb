5f262fc9b5b3f122430f6aeb24bee1ffbc2fc8b2
Alice Fonfa <alicefonfa@gmail.com>
2016-06-02 11:44:15 -0400

    added comment partial

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,20 @@
 post '/questions/:id/comments'
 	@question = Question.find_by(id: params[:id])
- 	@comment = Comment.new(body: params[:body])
+ 	@comment = @question.comments.build(body: params[:body], user: current_user)
  		if @comments.save
- 		erb :'comments/new'
+ 			redirect "/questions/#{@question.id}"
  		else 
+ 			@error = "Your comment in invalid."
+    		erb :'/questions/#{@question.id}'
 end
 
-put 'questions/:id/comments/:id'
-	@question = Question.find_by(id: params[:id])
- 	@comment = Comment.new(body: params[:body])
- 		if @comments.save
- 		erb :'comments/show'
- 		else 
-end
+# put 'questions/:id/comments/:id'
+# 	@question = Question.find_by(id: params[:id])
+#  	@comment = question.comments.find(id: params[:id])
+#  		if @comments.save
+#  		erb :'comments/show'
+#  		else 
+# end
 
 delete 'questions/:id/comments/:id'
   Comment.find_by(id: current_user.id).destroy
@@ -20,19 +22,21 @@ end
 
 post '/answer/:id/comments'
 	@answer = Answer.find_by(id: params[:id])
- 	@comment = Comment.new(body: params[:body])
+ 	@comment = @answer.comments.build(body: params[:body], users: current_user)
  		if @comments.save
- 		erb :'comments/new'
+ 		redirect "/answers/#{@answers.id}"
  		else 
+ 			@error = "Your comment in invalid."
+    		erb :'/answers/#{@answer.id}'
 end
 
-put '/answer/:id/comments/:id'
-	@answer = Answer.find_by(id: params[:id])
- 	@comment = Comment.new(body: params[:body])
- 		if @comments.save
- 		erb :'comments/show'
- 		else 
-end
+# put '/answer/:id/comments/:id'
+# 	@answer = Answer.find_by(id: params[:id])
+#  	@comment = answers.comments.find(id: params[:id])
+#  		if @comments.save
+#  		erb :'comments/show'
+#  		else 
+# end
 
 delete '/answer/:id/comments/:id'
 	Comment.find_by(id: current_user.id).destroy
