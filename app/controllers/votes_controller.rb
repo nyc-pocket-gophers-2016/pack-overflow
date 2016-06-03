e12d14362b5f9b985612de5b0201cdf1a22a8edf
notjoeyoo <joseph_yoo@brown.edu>
2016-06-03 15:05:45 -0400

    Work on question votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,8 +1,8 @@
 post '/questions/:id/votes' do
-	binding.pry
+	# binding.pry
 	@question = Question.find_by(id: params[:id])
  	@vote = @question.votes.build(value: params[:value], user: current_user)
- 	vote.save
+ 	@vote.save
  			#erb :'comment/new'
- 	erb :"/questions/#{@question.id}"
+ 	redirect :"/questions/#{@question.id}"
 end
